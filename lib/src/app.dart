12ae1a2ac942b47c879c725601eeb6020cbbaad7
ziho9593 <no1hjyo@naver.com>
2023-03-10 14:32:01 +0900

    :sparkles: BottomNavigation 히스토리 관리 및 앱 종료 팝업 생성
    BottomNavigation의 클릭 순서를 이용해 페이지 히스토리를 관리하도록 컨트롤러 설정 및 0번째 페이지에서 willPopAction() 실행 시 종료 확인 팝업을 띄울 수 있도록 생성

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -10,67 +10,66 @@ class App extends GetView<BottomNavController> {
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
-        child: Obx(
-          () => Scaffold(
-            appBar: AppBar(),
-            body: IndexedStack(index: controller.pageIndex.value, children: [
-              Container(
-                child: Center(child: Text("HOME")),
-              ),
-              Container(
-                child: Center(child: Text("SEARCH")),
-              ),
-              Container(
-                child: Center(child: Text("UPLOAD")),
-              ),
-              Container(
-                child: Center(child: Text("ACTIVITY")),
-              ),
-              Container(
-                child: Center(child: Text("MYPAGE")),
-              ),
-            ]),
-            bottomNavigationBar: BottomNavigationBar(
-                type: BottomNavigationBarType.fixed,
-                showSelectedLabels: false,
-                showUnselectedLabels: false,
-                currentIndex: controller.pageIndex.value,
-                onTap: controller.changeBottomNav,
-                elevation: 0,
-                items: [
-                  BottomNavigationBarItem(
-                    icon: Image.asset(IconsPath.homeOff),
-                    activeIcon: Image.asset(IconsPath.homeOn),
-                    label: 'home',
+      onWillPop: controller.willPopAction,
+      child: Obx(
+        () => Scaffold(
+          appBar: AppBar(),
+          body: IndexedStack(index: controller.pageIndex.value, children: [
+            Container(
+              child: Center(child: Text("HOME")),
+            ),
+            Container(
+              child: Center(child: Text("SEARCH")),
+            ),
+            Container(
+              child: Center(child: Text("UPLOAD")),
+            ),
+            Container(
+              child: Center(child: Text("ACTIVITY")),
+            ),
+            Container(
+              child: Center(child: Text("MYPAGE")),
+            ),
+          ]),
+          bottomNavigationBar: BottomNavigationBar(
+              type: BottomNavigationBarType.fixed,
+              showSelectedLabels: false,
+              showUnselectedLabels: false,
+              currentIndex: controller.pageIndex.value,
+              onTap: controller.changeBottomNav,
+              elevation: 0,
+              items: [
+                BottomNavigationBarItem(
+                  icon: Image.asset(IconsPath.homeOff),
+                  activeIcon: Image.asset(IconsPath.homeOn),
+                  label: 'home',
+                ),
+                BottomNavigationBarItem(
+                  icon: Image.asset(IconsPath.searchOff),
+                  activeIcon: Image.asset(IconsPath.searchOn),
+                  label: 'search',
+                ),
+                BottomNavigationBarItem(
+                  icon: Image.asset(IconsPath.uploadIcon),
+                  label: 'upload',
+                ),
+                BottomNavigationBarItem(
+                  icon: Image.asset(IconsPath.activeOff),
+                  activeIcon: Image.asset(IconsPath.activeOn),
+                  label: 'active',
+                ),
+                BottomNavigationBarItem(
+                  icon: Container(
+                    width: 40,
+                    height: 40,
+                    decoration: const BoxDecoration(
+                        shape: BoxShape.circle, color: Colors.grey),
                   ),
-                  BottomNavigationBarItem(
-                    icon: Image.asset(IconsPath.searchOff),
-                    activeIcon: Image.asset(IconsPath.searchOn),
-                    label: 'search',
-                  ),
-                  BottomNavigationBarItem(
-                    icon: Image.asset(IconsPath.uploadIcon),
-                    label: 'upload',
-                  ),
-                  BottomNavigationBarItem(
-                    icon: Image.asset(IconsPath.activeOff),
-                    activeIcon: Image.asset(IconsPath.activeOn),
-                    label: 'active',
-                  ),
-                  BottomNavigationBarItem(
-                    icon: Container(
-                      width: 40,
-                      height: 40,
-                      decoration: const BoxDecoration(
-                          shape: BoxShape.circle, color: Colors.grey),
-                    ),
-                    label: 'avarta',
-                  ),
-                ]),
-          ),
+                  label: 'avarta',
+                ),
+              ]),
         ),
-        onWillPop: () async {
-          return false;
-        });
+      ),
+    );
   }
 }
--- lib/src/controller/bottom_nav_controller.dart
+++ lib/src/controller/bottom_nav_controller.dart
@@ -1,3 +1,7 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_instagram_clone/src/components/message_popup.dart';
 import 'package:flutter_instagram_clone/src/pages/upload.dart';
 import 'package:get/get.dart';
 
@@ -5,8 +9,9 @@ enum PageName { HOME, SEARCH, UPLOAD, ACTIVITY, MYPAGE }
 
 class BottomNavController extends GetxController {
   RxInt pageIndex = 0.obs;
+  List<int> bottomHistory = [0];
 
-  void changeBottomNav(int value) {
+  void changeBottomNav(int value, {bool hasGesture = true}) {
     var page = PageName.values[value];
     switch (page) {
       case PageName.UPLOAD:
@@ -16,12 +21,40 @@ class BottomNavController extends GetxController {
       case PageName.SEARCH:
       case PageName.ACTIVITY:
       case PageName.MYPAGE:
-        _changePage(value);
+        _changePage(value, hasGesture: hasGesture);
         break;
     }
   }
 
-  void _changePage(int value) {
+  void _changePage(int value, {bool hasGesture = true}) {
     pageIndex(value);
+    if (!hasGesture) return;
+    if (bottomHistory.last != value) {
+      bottomHistory.add(value);
+    }
+  }
+
+  Future<bool> willPopAction() async {
+    if (bottomHistory.length == 1) {
+      showDialog(
+        context: Get.context!,
+        builder: (context) => MessagePopup(
+          title: '시스템',
+          message: '종료하시겠습니까?',
+          okCallback: () {
+            exit(0);
+          },
+          cancelCallback: () {
+            Get.back();
+          },
+        ),
+      );
+      return true;
+    } else {
+      bottomHistory.removeLast();
+      var idx = bottomHistory.last;
+      changeBottomNav(idx, hasGesture: false);
+      return false;
+    }
   }
 }
