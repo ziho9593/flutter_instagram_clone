aa194ac436cbe616bfaea6d90e060e71de105f76
ziho9593 <ziho9593@gmail.com>
2023-03-13 15:18:26 +0900

    :lipstick: 업로드 페이지 UI 구현 - 전체적으로 레이아웃만 잡는 작업 진행 - 선택한 이미지를 보여주는 _imagePreview() - 프리뷰와 그리드뷰 사이에 _header() - 앨범의 사진 목록을 보여주는 _imageSelectList()

--- lib/src/pages/upload.dart
+++ lib/src/pages/upload.dart
@@ -1,12 +1,143 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_instagram_clone/src/components/image_data.dart';
+import 'package:get/get.dart';
 
 class Upload extends StatelessWidget {
   const Upload({super.key});
 
+  Widget _imagePreview() {
+    return Container(
+      width: Get.width,
+      height: Get.width,
+      color: Colors.grey,
+    );
+  }
+
+  Widget _header() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: Row(
+              children: const [
+                Text(
+                  '갤러리',
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.black,
+                  ),
+                ),
+                Icon(Icons.arrow_drop_down)
+              ],
+            ),
+          ),
+          Row(
+            children: [
+              Container(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                decoration: BoxDecoration(
+                  color: const Color(0xff808080),
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: Row(
+                  children: [
+                    ImageData(IconsPath.imageSelectIcon),
+                    const SizedBox(width: 7),
+                    const Text(
+                      '여러 항목 선택',
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(width: 5),
+              Container(
+                padding: const EdgeInsets.all(6),
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Color(0xff808080),
+                ),
+                child: ImageData(IconsPath.cameraIcon),
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _imageSelectList() {
+    return GridView.builder(
+        // SingleChildScrollView와 GridView의 스크롤 영역이 겹치면 에러가 발생하기 때문에, physics와 shrinkWrap을 설정
+        physics: const NeverScrollableScrollPhysics(),
+        shrinkWrap: true,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 4, // 열의 갯수를 지정
+            childAspectRatio: 1, // 1로 지정하면 정사각형 모양
+            mainAxisSpacing: 1,
+            crossAxisSpacing: 1),
+        itemCount: 100,
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            color: Colors.red,
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.red,
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: GestureDetector(
+          onTap: () {},
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: ImageData(
+              IconsPath.closeImage,
+            ),
+          ),
+        ),
+        centerTitle: false,
+        title: const Text(
+          '새 게시물',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
+            color: Colors.black,
+          ),
+        ),
+        actions: [
+          GestureDetector(
+            onTap: () {},
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: ImageData(
+                IconsPath.nextImage,
+                width: 50,
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            _imagePreview(),
+            _header(),
+            _imageSelectList(),
+          ],
+        ),
+      ),
     );
   }
 }
