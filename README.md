dafa6205d109c15b4393fd6fa3a40786ec9b5a03
ziho9593 <ziho9593@gmail.com>
2023-03-16 17:14:16 +0900

    :sparkles: 파이어베이스 및 구글 로그인 세팅 Firebase CLI를 이용한 파이어베이스 세팅 구글 로그인을 활용한 로그인 화면 세팅

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 
 ## Stacks
 <img src="https://img.shields.io/badge/Flutter 3.7.3-02569B?style=plastic&logo=flutter">&nbsp;
-<img src="https://img.shields.io/badge/Dart 2.19.2-0175C2?style=plastic&logo=dart">&nbsp;&nbsp;
+<img src="https://img.shields.io/badge/Dart 2.19.2-0175C2?style=plastic&logo=dart">&nbsp;
 <img src="https://img.shields.io/badge/Firebase-FFCA28?style=plastic&logo=firebase">&nbsp;
 
 ## Commit Convention
@@ -16,3 +16,4 @@
 - 📝 memo - **Add or update documentation.**
 - ➕ heavy_plus_sign - **Add a dependency.**
 - ➖ heavy_minus_sign - **Remove a dependency.**
+- 🔧 wrench - **Add or update configuration files.**
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_instagram_clone/src/app.dart';
+import 'package:flutter_instagram_clone/firebase_options.dart';
 import 'package:flutter_instagram_clone/src/binding/init_bindings.dart';
+import 'package:flutter_instagram_clone/src/root.dart';
 import 'package:get/get.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    name: 'jiho-insta-clone',
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -23,7 +30,7 @@ class MyApp extends StatelessWidget {
         ),
       ),
       initialBinding: InitBinding(),
-      home: const App(),
+      home: const Root(),
     );
   }
 }
